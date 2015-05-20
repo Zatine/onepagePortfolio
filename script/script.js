59d86362cf4343eb6d704139cfd971f208f4e7a0
Mikaela Grundin <me@mikaelagrundin.com>
2015-05-20 23:36:17 +0200

    Mobile Version
    Made the site more responsive and changed some styling.

--- script/script.js
+++ script/script.js
@@ -125,7 +125,26 @@ xhr.onreadystatechange = function(){
 	else if(xhr.status === 404){
 		document.querySelector('.projects').innerHTML = "There was a problem retrieving the files! Please try again.";
 	}
-}
+};
 xhr.open("GET", "data/projects.json");
 xhr.send();
 
+//SCROLLING ANIMATION
+function runScroll() {
+	scrollTo(document.body, 0, 600);
+}
+var scrollme;
+scrollme = document.querySelector("#scrollme");
+scrollme.addEventListener("click",runScroll,false)
+
+function scrollTo(element, to, duration) {
+	if (duration < 0) return;
+	var difference = to - element.scrollTop;
+	var perTick = difference / duration * 10;
+
+	setTimeout(function() {
+		element.scrollTop = element.scrollTop + perTick;
+		if (element.scrollTop == to) return;
+		scrollTo(element, to, duration - 10);
+	}, 10);
+}
